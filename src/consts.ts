e207c226560358481b2440ca4485d9ff34a05059
Behrad Saraiyeh <b.saraiyeh@gmail.com>
2026-03-20 00:37:35 +0330

    content: site description updated

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Behrad Saraiyeh: Work & Insights";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "I’m an SRE and DevOps engineer with a strong background in backend development. I love learning and writing, and I share things I learn from my work here.";
